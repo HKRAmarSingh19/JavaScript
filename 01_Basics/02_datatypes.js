bb337c8af94bc9ef75dc3cf51b203eef52c4cfcf
HKR Amar Singh <hkramarsingh@gmail.com>
2026-09-22 19:02:08 +0530

    commit

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all js code as never version
+// alert("Hello") // we are using nodejs , not browser
+console.log(3+3)
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,13 @@
 console.log("Hello Hkr Amar Singh")
+const accountID = 12345;
+let accountEmail = "Hkramarsingh@gmail.com"
+var accountPassword = "12345"
+console.log(accountEmail);
+
+console.table([accountID,accountEmail,accountPassword])
+
+// Never use var for varibale declaration 
+hkr = "hkr"
+console.log(hkr);
+
+
